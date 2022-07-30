275afc0d0ca69aaec73c4ba1976e00cb82b5ac43
Daria Grekova <dg94@bk.ru>
2022-07-30 13:36:38 +0300

    Стили, Отриовка в цикле/директива v-for, создание нового поста, двустороннее связывание, модификаторы stop/prevent

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,84 @@
 <template>
-  <div>
-    <button v-on:click="addLike">Like</button>
-    <button @click="addDislike">Dislike</button>
-    <div>Кол-во лайков: <strong>{{ likes }}</strong></div> 
-    <div>Кол-во дизлайков: <strong>{{ dislikes }}</strong></div> 
+  <div class="app">
+    <form @submit.prevent>
+      <h4>Создание поста</h4>
+      <input v-bind:value="title" @input="title = $event.target.value" class="input" type="text" placeholder="Название">
+      <input v-bind:value="body" @input="body = $event.target.value" class="input" type="text" placeholder="Описание">
+      <button class="btn" @click="createPost">Создать</button>
+    </form>
+    <div class="post" v-for="post in posts">
+      <div><strong>Название:</strong> {{post.title}}</div>
+      <div><strong>Описание:</strong> {{post.body}}</div>
+    </div>
   </div>
 </template>
 
 <script>
-export default {
-  data() {
-    return {
-      likes: 0,
-      dislikes: 0,
-    }
-  },
-  methods: {
-    addLike() {
-      this.likes += 1;
+  export default {
+    data() {
+      return {
+        posts: [
+          {id: 1, title: 'JavaScript', body: 'Описание поста'},
+          {id: 2, title: 'JavaScript 2', body: 'Описание поста 2'},
+          {id: 3, title: 'JavaScript 3', body: 'Описание поста 3'},
+          {id: 4, title: 'JavaScript 4', body: 'Описание поста 4'},
+        ],
+        title: '',
+        body: ''
+      }
     },
-        addDislike() {
-      this.dislikes += 1;
+    methods: {
+      createPost() {
+        const newPost = {
+          id: Date.now(),
+          title: this.title,
+          body: this.body
+        }
+        this.posts.push(newPost);
+        this.title = '',
+        this.body = ''
+      }
     }
+
   }
-}
 
 </script>
 
 <style>
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  .app {
+    padding: 20px;
+  }
+
+  form {
+    display: flex;
+    flex-direction: column;
+  }
+
+  .post {
+    margin-top: 15px;
+    padding: 15px;
+    border: 2px solid teal;
+  }
+
+  .input {
+    width: 100%;
+    padding: 10px 15px;
+    margin-top: 15px;
+    border: 1px solid teal;
+  }
+
+  .btn {
+    margin-top: 15px;
+    padding: 10px 15px;
+    align-self: flex-end;
+    background: none;
+    color: teal;
+    border: 1px solid teal;
+  }
 </style>
