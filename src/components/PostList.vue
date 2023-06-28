dc5858ebb7dc01bdab392f530c42c26b1cb71794
daria-grek <dg94@bk.ru>
2023-06-28 21:26:40 +0300

    декомпозиция (пост в отдельный компонент)

--- src/components/PostList.vue
+++ src/components/PostList.vue
@@ -1,27 +1,27 @@
 <template>
-  <div class="post" v-for="post in posts" v-bind:key="post">
-    <div><strong>Название:</strong> {{post.title}}</div>
-    <div><strong>Описание:</strong> {{post.body}}</div>
+  <div class="">
+    <h3>Список пользователей</h3>
+    <post-item v-for="post in posts" :post="post" :key="post.id" />
   </div>
 </template>
 
 <script>
+import PostItem from "./PostItem.vue";
 export default {
+  components: { PostItem },
   props: {
     posts: {
       type: Array,
       required: true,
-    }
-  }
-}
+    },
+  },
+};
 </script>
 
 <style scoped>
-
-  .post {
-    margin-top: 15px;
-    padding: 15px;
-    border: 2px solid teal;
-  }
-
+.post {
+  margin-top: 15px;
+  padding: 15px;
+  border: 2px solid teal;
+}
 </style>
